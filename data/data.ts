84a84b7369ae580ab026078247b72f2c03e760bc
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-03-19 17:35:35 +0900

    grab json in JS

--- data/data.ts
+++ data/data.ts
@@ -3,10 +3,43 @@ import {
   melpaDownloadCountsJson,
   elpaConvertedJson,
 } from "./schema.ts";
-import { readFileSync } from "node:fs";
+import { readFileSync, writeFileSync } from "node:fs";
+import { $ } from "zx";
 
-async function getJson(url: string) {
+/**
+ * Fetch ELPA archive-contents from `url`, convert it to JSON, then return the value.
+ * The conversion step runs Emacs to do the job.
+ * If `cachePath` is provided, try to read from it. Writing to it is not
+ * supported yet.
+ */
+async function getElpaJson(url: string, cachePath?: string) {
+  if (cachePath) {
+    try {
+      return elpaConvertedJson.parse(
+        JSON.parse(readFileSync(cachePath, { encoding: "utf-8" }))
+      );
+    } catch {}
+  }
+  const stdout = (await $`sh ./elpa-to-json - ${url}`).stdout;
+  // We can't use ProcessOutput.json() because that merges stdout and stderr.
+  return elpaConvertedJson.parse(JSON.parse(stdout));
+}
+
+/**
+ * Wrapper for `fetch` on `url`.
+ * If `cachePath` is provided, try to read from it; and write to it if it
+ * doesn't exist.
+ */
+async function getJson(url: string, cachePath?: string) {
+  if (cachePath) {
+    try {
+      return JSON.parse(readFileSync(cachePath, { encoding: "utf-8" }));
+    } catch {}
+  }
   const response = await fetch(url);
+  if (cachePath) {
+    writeFileSync(cachePath, await response.text());
+  }
   return await response.json();
 }
 
@@ -17,11 +50,24 @@ async function getJson(url: string) {
 //   already in URL within archive.json, and we don't need the file paths.
 // - download_counts.json is as the name says. It is an object of names to numbers.
 export const melpaArchive = melpaArchiveJson.parse(
-  await getJson("https://melpa.org/archive.json")
+  await getJson("https://melpa.org/archive.json", "melpa-archive.json")
 );
 export const melpaDownloads = melpaDownloadCountsJson.parse(
-  await getJson("https://melpa.org/download_counts.json")
+  await getJson(
+    "https://melpa.org/download_counts.json",
+    "melpa-download-counts.json"
+  )
 );
-export const gnuElpa = elpaConvertedJson.parse(
-  JSON.parse(readFileSync("./gnu.json", { encoding: "utf-8" }))
+export const gnu = await getElpaJson(
+  "https://elpa.gnu.org/packages/",
+  "gnu.json"
+);
+export const nongnu = await getElpaJson(
+  "https://elpa.nongnu.org/nongnu/",
+  "nongnu.json"
+);
+export const org = await getElpaJson("https://orgmode.org/elpa/", "org.json");
+export const jcs = await getElpaJson(
+  "https://jcs-emacs.github.io/jcs-elpa/packages/",
+  "jcs-elpa.json"
 );
