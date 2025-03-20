566cccc4c90e80a3e45fd3c19d9ef7047a6d5c35
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-03-20 21:03:40 +0900

    try out vue, try using svelte-table-pagination
    ultimately using tanstack table would probably be best; but it doesn't
    support svelte 5 yet. great. (sigh)
    
    meanwhile svelte-table-pagination works surprisingly easily.

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import vue from "@astrojs/vue";
+
+import svelte from "@astrojs/svelte";
 
 export default defineConfig({
+  integrations: [vue(), svelte()],
   vite: {
     clearScreen: false,
     plugins: [tailwindcss()],
