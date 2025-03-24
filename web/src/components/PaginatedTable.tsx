c469b4a60a53bcd7beb6cf31ffd247131094df01
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-03-24 15:50:50 +0900

    Start hooking things up

--- web/src/components/PaginatedTable.tsx
+++ web/src/components/PaginatedTable.tsx
@@ -1,2 +1,96 @@
-import { useReactTable, getCoreRowModel } from "@tanstack/react-table";
+import {
+  useReactTable,
+  getCoreRowModel,
+  getPaginationRowModel,
+  flexRender,
+  createColumnHelper,
+  type Header,
+  type SortDirection,
+  type PaginationState,
+} from "@tanstack/react-table";
 import type { Pkg } from "$data/schema.ts";
+import { useState } from "react";
+
+const columnHelper = createColumnHelper<Pkg>();
+const columns = [
+  columnHelper.accessor("name", {
+    cell: (info) => info.getValue(),
+  }),
+];
+
+function SortIndicator({ status }: { status: SortDirection | false }) {
+  if (status === "asc") {
+    return <i className="bi bi-chevron-up"></i>;
+  } else if (status === "desc") {
+    return <i className="bi bi-chevron-down"></i>;
+  } else {
+    return null;
+  }
+}
+
+function Header<TData, TValue>({ header }: { header: Header<TData, TValue> }) {
+  if (header.isPlaceholder) return null;
+  return (
+    <div>
+      {flexRender(header.column.columnDef.header, header.getContext())}
+      <SortIndicator status={header.column.getIsSorted()} />
+    </div>
+  );
+}
+
+export default ({ data }: { data: Pkg[] }) => {
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 10,
+  } as PaginationState);
+  const table = useReactTable({
+    data: data,
+    columns: columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onPaginationChange: setPagination,
+    state: { pagination: pagination },
+  });
+  return (
+    <table>
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th key={header.id}>
+                <Header header={header} />
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <td key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+      <tfoot>
+        {table.getFooterGroups().map((footerGroup) => (
+          <tr key={footerGroup.id}>
+            {footerGroup.headers.map((header) => (
+              <th key={header.id}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.footer,
+                      header.getContext(),
+                    )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </tfoot>
+    </table>
+  );
+};
