b98142e953cbd1a57d8522781be7980da5a51b0f
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-04-06 00:44:46 +0900

    Clean up the rest of the infrastructure for React

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import react from "@astrojs/react";
 import solidJs from "@astrojs/solid-js";
 
 export default defineConfig({
-  integrations: [react(), solidJs()],
+  integrations: [solidJs()],
   vite: {
     clearScreen: false,
     plugins: [tailwindcss()],
