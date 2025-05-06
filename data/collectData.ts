c6f8e77065b7a23d60a27b7ba4a0ad4912ef833e
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-05-07 02:11:03 +0900

    last while's work

--- data/collectData.ts
+++ data/collectData.ts
@@ -4,10 +4,21 @@ import {
   elpaConvertedJson,
   melpaRecipesJson,
   melpaRecipeToUrl,
+  epkgsSqlBuiltinPackages,
+  stringOfJsonString,
+  epkgsSqlPeople,
+  epkgsSqlStrings,
 } from "./schema.ts";
-import type { Pkg } from "./schema.ts";
+import type { EpkgsSqlPeople, Pkg } from "./schema.ts";
+import { Database } from "bun:sqlite";
 import { readFileSync, writeFileSync, mkdirSync } from "node:fs";
 import { $ } from "zx";
+
+const epkgs = new Database("cache/epkgs.sqlite", {
+  readonly: true,
+  create: false,
+  strict: true,
+});
 
 /**
  * Fetch ELPA archive-contents from `url`, convert it to JSON, then return the value.
@@ -110,64 +121,166 @@ const elpas = {
 
 const packages: Pkg[] = [];
 
-for (const [archiveName, pkgs] of Object.entries(elpas)) {
-  for (const [pkgName, [version, deps, desc, _kind, props]] of Object.entries(
-    pkgs,
+function insertElpas() {
+  for (const [archiveName, pkgs] of Object.entries(elpas)) {
+    for (const [pkgName, [version, deps, desc, _kind, props]] of Object.entries(
+      pkgs,
+    )) {
+      const newPkg: Pkg = {
+        name: pkgName,
+        archive: archiveName,
+        ver: version,
+        deps: deps,
+        summary: desc,
+      };
+      // Not misspell, the input is always singular while we want output to be plural
+      if (props?.maintainer) newPkg.maintainers = props.maintainer;
+      if (props?.authors) newPkg.authors = props.authors;
+      if (props?.keywords) newPkg.keywords = props.keywords;
+      if (props?.commit) newPkg.commit = props.commit;
+      if (props?.url) newPkg.url = props.url;
+
+      packages.push(newPkg);
+    }
+  }
+}
+
+function insertMelpas() {
+  for (const [archiveName, { archive, downloads, recipes }] of Object.entries(
+    melpas,
   )) {
-    const newPkg: Pkg = {
-      name: pkgName,
-      archive: archiveName,
-      ver: version,
-      deps: deps,
-      summary: desc,
-    };
-    // Not misspell, the input is always singular while we want output to be plural
-    if (props?.maintainer) newPkg.maintainers = props.maintainer;
-    if (props?.authors) newPkg.authors = props.authors;
-    if (props?.keywords) newPkg.keywords = props.keywords;
-    if (props?.commit) newPkg.commit = props.commit;
-    if (props?.url) newPkg.url = props.url;
+    for (const [pkgName, { ver, deps, desc, props }] of Object.entries(
+      archive,
+    )) {
+      const newPkg: Pkg = {
+        name: pkgName,
+        archive: archiveName,
+        ver: ver,
+        deps: deps,
+        summary: desc,
+      };
+      if (downloads[pkgName]) {
+        newPkg.downloads = downloads[pkgName];
+      }
+      if (props.maintainers) newPkg.maintainers = props.maintainers;
+      if (props.authors) newPkg.authors = props.authors;
+      if (props.keywords) newPkg.keywords = props.keywords;
+      if (props.commit) newPkg.commit = props.commit;
+      // throwing away revdesc
 
+      const recipe = recipes[pkgName];
+      if (recipe) {
+        const recipeUrl = melpaRecipeToUrl(recipe);
+        newPkg.url = recipeUrl;
+      } else if (props.url) {
+        // fall back to the URL the package itself declares
+        console.log(
+          `${pkgName} has no URL in its recipe, somehow. Falling back to ${props.url}`,
+        );
+        newPkg.url = props.url;
+      }
+
+      packages.push(newPkg);
+    }
+  }
+}
+
+function epkgsPeopleToStrings(people: EpkgsSqlPeople) {
+  return people.map(({ name, email }) => {
+    const parsedName =
+      typeof name === "string" ? stringOfJsonString.parse(name) : name;
+    const parsedEmail =
+      typeof email === "string" ? stringOfJsonString.parse(email) : email;
+    const namePresent = typeof parsedName === "string";
+    const emailPresent = typeof parsedEmail === "string";
+    if (namePresent && emailPresent) {
+      return `${parsedName} <${parsedEmail}>`;
+    }
+    if (namePresent) {
+      return parsedName;
+    }
+    if (emailPresent) {
+      return parsedEmail;
+    }
+    // This should never happen: either name or email should be present.
+    return "";
+  });
+}
+
+function insertEpkgsBuiltins() {
+  for (const epkgsBuiltinPackage of epkgsSqlBuiltinPackages.parse(
+    epkgs
+      .query(
+        `
+SELECT name, library, homepage, summary, commentary
+FROM packages
+WHERE class = 'builtin'
+  AND name != '"emacs"'`,
+      )
+      .all(),
+  )) {
+    const { name } = epkgsBuiltinPackage;
+    const keywords = epkgsSqlStrings.parse(
+      epkgs
+        .query(
+          `
+SELECT keyword FROM keywords
+WHERE package = ?`,
+        )
+        .values(name)
+        .map((v: unknown[]) => v[0]),
+    );
+    const authors = epkgsSqlPeople.parse(
+      epkgs
+        .query(
+          `
+SELECT name, email FROM authors
+WHERE package = ?`,
+        )
+        .all(name),
+    );
+    const maintainers = epkgsSqlPeople.parse(
+      epkgs
+        .query(
+          `
+SELECT name, email FROM maintainers
+WHERE package = ?`,
+        )
+        .all(name),
+    );
+    const deps = epkgsSqlStrings.parse(
+      epkgs
+        .query(
+          `
+SELECT package FROM provided
+WHERE feature IN (
+  SELECT feature FROM required
+  WHERE package = ?
+)
+`,
+        )
+        .values(name)
+        .map((v: unknown[]) => v[0]),
+    );
+    console.log(name);
+    const newPkg: Pkg = {
+      name: stringOfJsonString.parse(name),
+      archive: "builtin",
+      ver: [],
+      // Forcibly give it an empty versionList
+      deps: Object.fromEntries(deps.map((dep) => [dep, []])),
+      summary: stringOfJsonString.parse(epkgsBuiltinPackage.summary),
+      maintainers: epkgsPeopleToStrings(maintainers),
+      authors: epkgsPeopleToStrings(authors),
+      keywords: keywords,
+    };
     packages.push(newPkg);
   }
 }
 
-for (const [archiveName, { archive, downloads, recipes }] of Object.entries(
-  melpas,
-)) {
-  for (const [pkgName, { ver, deps, desc, props }] of Object.entries(archive)) {
-    const newPkg: Pkg = {
-      name: pkgName,
-      archive: archiveName,
-      ver: ver,
-      deps: deps,
-      summary: desc,
-    };
-    if (downloads[pkgName]) {
-      newPkg.downloads = downloads[pkgName];
-    }
-    if (props.maintainers) newPkg.maintainers = props.maintainers;
-    if (props.authors) newPkg.authors = props.authors;
-    if (props.keywords) newPkg.keywords = props.keywords;
-    if (props.commit) newPkg.commit = props.commit;
-    // throwing away revdesc
-
-    const recipe = recipes[pkgName];
-    if (recipe) {
-      const recipeUrl = melpaRecipeToUrl(recipe);
-      newPkg.url = recipeUrl;
-    } else if (props.url) {
-      // fall back to the URL the package itself declares
-      console.log(
-        `${pkgName} has no URL in its recipe, somehow. Falling back to ${props.url}`,
-      );
-      newPkg.url = props.url;
-    }
-
-    packages.push(newPkg);
-  }
-}
-
+insertElpas();
+insertMelpas();
+insertEpkgsBuiltins();
 writeFileSync(
   "combined.json",
   JSON.stringify({ collectedDate: new Date(), packages: packages }, null, 1),
