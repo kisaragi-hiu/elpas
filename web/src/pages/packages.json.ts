ba361ff8a546ebb191e90a7f5a09a59018b16ed9
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-05-07 17:14:19 +0900

    Use the actual export

--- web/src/pages/packages.json.ts
+++ web/src/pages/packages.json.ts
@@ -1,5 +1,5 @@
 import type { APIRoute } from "astro";
-import { combined } from "$data/data.ts"
+import { packages } from "$data/data.ts"
 export const GET: APIRoute = async ({ params }) => {
-  return new Response(JSON.stringify(combined));
+  return new Response(JSON.stringify(packages));
 };
