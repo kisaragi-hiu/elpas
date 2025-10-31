0fb2dafb78f86068ec19c74fc8a53eac7405573b
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-10-31 23:14:40 +0900

    try to speed up epkg loading with a stream
    maybe the bottleneck is reading the whole file into memory? I dunno.
    This may not even work, since db.run creates its own statement.
    
    Edit: yes, that was effective, at least in perf. On GitHub Actions it
    took about 1 secons (or less) instead of 20 woth the previous approach.
    There were no errors, and the resulting row count is the same, so this
    is probably a success.

--- data/load-epkgs.ts
+++ data/load-epkgs.ts
@@ -1,3 +1,13 @@
 import { Database } from "bun:sqlite";
+import { createReadStream } from "node:fs";
+import { createInterface } from "node:readline";
+
+// readline can read lines from a Node stream
+// no support for web streams, unfortunately
 const db = new Database("cache/epkgs.sqlite");
-db.run(await Bun.file("./cache/epkg.sql").text());
+for await (const line of createInterface({
+  input: createReadStream("./cache/epkg.sql"),
+  crlfDelay: Infinity,
+})) {
+  db.run(line);
+}
