8693cf5142d6dd11be8ddad818854659982166e0
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-11-17 04:33:18 +0900

    Update packages

--- web/src/pages/packages.json.ts
+++ web/src/pages/packages.json.ts
@@ -1,5 +1,5 @@
 import type { APIRoute } from "astro";
-import { packages } from "$data/data.ts"
+import { packages } from "$data/data.ts";
 export const GET: APIRoute = async ({ params }) => {
   return new Response(JSON.stringify(packages));
 };
